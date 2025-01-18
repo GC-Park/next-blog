38d0317097f08e4bb951d0e800ace1479bd29070
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-01-18 01:10:24 +0100

    refactor: modify drinks page

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,23 @@
-const DrinksPages = () => {
-  return <h1 className="text-7xl">DrinksPages</h1>;
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+import DrinksList from "@/components/DrinksList";
+
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch drinks...");
+  }
+  const data = await response.json();
+  return data;
 };
-export default DrinksPages;
+
+const ServerComponent = async () => {
+  const data = await fetchDrinks();
+  return (
+    <div>
+      <DrinksList drinks={data.drinks} />
+    </div>
+  );
+};
+export default ServerComponent;
