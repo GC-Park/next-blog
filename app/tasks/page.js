09e0435ae5556ab39c4646d00d76a60c679bbe3c
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-01-25 23:53:09 +0100

    refactor: modify TaskFormCustom

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,10 +1,10 @@
-import TaskFormCustom from "@/components/TaskFormCustom";
+import TaskForm from "@/components/TaskFormCustom";
 import TaskList from "@/components/TaskList";
 
 const TasksPage = () => {
   return (
     <div className="max-w-lg">
-      <TaskFormCustom />
+      <TaskForm />
       <TaskList />
     </div>
   );
