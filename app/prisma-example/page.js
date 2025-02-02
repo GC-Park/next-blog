d58f556d8b7f13cdb33bbd091648abde602713cc
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-02-02 20:19:49 +0100

    refactor: modify PrismaExample

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,25 +1,25 @@
 import prisma from "@/utils/db";
 
-const prismaHandlers = async () => {
-  await prisma.task.create({
-    data: {
-      content: "wake up",
-    },
-  });
-  const allTasks = await prisma.task.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
 
-  return allTasks;
+const prismaHandlers = async () => {
+  console.log("prisma example");
+  // await prisma.task.create({
+  //   data: {
+  //     content: 'wake up',
+  //   },
+  // });
+  return prisma.task.findMany();
 };
 
-const Prisma = async () => {
+const PrismaExample = async () => {
   const tasks = await prismaHandlers();
+  if (tasks.length === 0) {
+    return <h2 className="mt-8 font-medium text-lg">No tasks to show...</h2>;
+  }
+
   return (
     <div>
-      <h1 className="text-3xl font-medium mb-4">Prisma Example </h1>
+      <h1 className="text-7xl">PrismaExample</h1>
       {tasks.map((task) => {
         return (
           <h2 key={task.id} className="text-xl py-2">
@@ -30,4 +30,4 @@ const Prisma = async () => {
     </div>
   );
 };
-export default Prisma;
+export default PrismaExample;
