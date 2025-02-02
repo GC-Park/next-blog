0ffd5ba48b8dc3f21d1834b725417750d6755384
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-02-02 18:56:05 +0100

    refactor: modify AboutInfoPage

--- app/about/info/page.js
+++ app/about/info/page.js
@@ -1,22 +1,8 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Next.js Tutorial",
-  description: "Build awesome stuff with Next.js!",
-};
-
-export default function RootLayout({ children }) {
+const AboutInfoPage = () => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main className="px-8 py-20 max-w-6xl mx-auto ">{children}</main>
-      </body>
-    </html>
+    <div>
+      <h1 className="text-7xl">AboutInfoPage</h1>
+    </div>
   );
-}
+};
+export default AboutInfoPage;
