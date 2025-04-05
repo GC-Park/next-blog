17d1278895afa4ed7a216d3a95682bfbe29f7fd2
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-04-06 01:06:58 +0200

    refactor: update NewPostPage

--- app/blog/new/page.js
+++ app/blog/new/page.js
@@ -1,7 +1,20 @@
 import Link from "next/link";
 import PostForm from "@/components/blog/PostForm";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
 
-const NewPostPage = () => {
+const NewPostPage = async () => {
+  const user = await currentUser();
+
+  const isAdmin =
+    user?.emailAddresses?.find(
+      (email) => email.id === user.primaryEmailAddressId
+    )?.emailAddress === process.env.ADMIN_EMAIL;
+
+  if (!isAdmin) {
+    redirect("/blog");
+  }
+
   return (
     <div className="max-w-3xl mx-auto">
       <div className="mb-8">
@@ -9,9 +22,9 @@ const NewPostPage = () => {
           ← 돌아가기
         </Link>
       </div>
-      
+
       <h1 className="text-3xl font-bold mb-6">새 글 작성</h1>
-      
+
       <PostForm />
     </div>
   );
