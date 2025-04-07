aa2d494f842814f373c1a4caf5a1ed23d02382fb
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-04-07 23:09:06 +0200

    refactor: modify DeleteForm

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,8 +1,9 @@
 import { deleteTask } from "@/utils/actions";
 
-const DeleteForm = ({ id }) => {
+const DeleteForm = ({ id, selectedDate }) => {
   return (
     <form action={deleteTask}>
+      <input type="hidden" name="date" value={selectedDate} />
       <input type="hidden" name="id" value={id} />
       <button className="btn btn-error btn-xs">delete</button>
     </form>
