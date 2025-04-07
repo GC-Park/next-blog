8dd4a0a4c9f241a5ba975a3219738a099b0f4c97
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-04-07 23:10:08 +0200

    refactor: modify TaskPage

--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -1,15 +1,17 @@
 import EditForm from "@/components/EditForm";
 import { getTask } from "@/utils/actions";
 import Link from "next/link";
+import { format } from "date-fns";
 
 const TaskPage = async ({ params }) => {
   const task = await getTask(params.id);
+  const formattedDate = format(task.date, "yyyy-MM-dd");
 
   return (
     <>
       <div className="mb-16">
-        <Link href="/tasks" className="btn btn-accent">
-          Back to Tasks
+        <Link href={`/tasks/date/${formattedDate}`} className="btn btn-accent">
+          돌아가기
         </Link>
       </div>
       <EditForm task={task} />
