0fbb2f8484c6d840f3520aabb133233b5522b01d
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-04-18 22:04:00 +0200

    refactor: change home page title

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+      <h1 className="text-5xl mb-8 font-bold">GC-PARK BLOG</h1>
       <Link href="/client" className="btn btn-accent">
         get started
       </Link>
