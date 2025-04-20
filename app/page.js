8c9786b9fcc4e71e1d074ead881ca89812f27f9b
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-04-20 15:16:16 +0200

    refactor: change get start link herf

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-5xl mb-8 font-bold">GC-PARK BLOG</h1>
-      <Link href="/client" className="btn btn-accent">
+      <Link href="/blog" className="btn btn-accent">
         get started
       </Link>
     </div>
