9277a26f7cbd42add98bb37876e0ff65476f61cc
GeunCheolPark <geuncheol.park@deliveryhero.com>
2025-05-19 22:59:24 +0200

    refactor: change isPublicRoute

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/blog(.*)",
+  "/aboutMe(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
